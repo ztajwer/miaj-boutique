f32396971084549816382f47f24b89d0b32afdfc
Zimal Tajwer <zimaltajwer@gmail.com>
2026-08-11 14:21:55 +0500

    load

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -13,8 +13,8 @@ interface LoaderProps {
   onComplete: () => void;
 }
 
-const LOADER_DURATION_MS_DEFAULT = 20000;
-const LOADER_DURATION_MS_LOW = 20000;
+const LOADER_DURATION_MS_DEFAULT = 7000;
+const LOADER_DURATION_MS_LOW = 7000;
 const FADE_DURATION_MS = 300;
 
 export default function Loader({ onComplete }: LoaderProps) {
