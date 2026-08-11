47f4edd4afae8116da25f0025a3470d03a7eb66f
Zimal Tajwer <zimaltajwer@gmail.com>
2026-08-11 17:22:50 +0500

    j

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -112,8 +112,6 @@ export default function RootLayout({
         <link rel="preload" href="/main_mob_bg.png" as="image" />
         <link rel="preload" href="/logo_outline.png" as="image" />
         <link rel="preload" href="/logo.png" as="image" />
-        <link rel="preload" href="/Kiosk_Centre.glb" as="fetch" crossOrigin="anonymous" />
-        <link rel="preload" href="/door_col.glb" as="fetch" crossOrigin="anonymous" />
       </head>
       <body className="font-sans antialiased" suppressHydrationWarning>
         <CustomizationProvider>
